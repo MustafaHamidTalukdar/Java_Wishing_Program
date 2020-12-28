2af1e29671e514ac038a42006dd895fcadc5d991
Mustafa Hamid Talukdar <68769724+MustafaHamidTalukdar@users.noreply.github.com>
2020-12-28 16:45:52 +0530

    Add files via upload

---
+++ Java_03.java
@@ -0,0 +1,19 @@
+/***********************************
+ ***************@MUSTAFA************
+ **********************************/
+
+package com.company;
+
+// Importing Scanner
+import java.util.Scanner;
+
+public class Java_03 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What Is Your Name");
+        String Name = sc.next();
+        System.out.println("Hello "+Name+" Have A Good Day");
+    }
+}
